7137a3db518eab5dad923c710648c24bbc737058
anujp10 <aspatil@usc.edu>
2020-02-09 23:10:36 -0800

    Problem_6

--- tests/solution_test.cc
+++ tests/solution_test.cc
@@ -1,10 +1,3 @@
 #include "src/lib/solution.h"
 #include "gtest/gtest.h"
 #include <vector>
-
-TEST(HelloWorldShould, ReturnHelloWorld) {
-  Solution solution;
-  std::string actual = solution.PrintHelloWorld();
-  std::string expected = "**** Hello World ****";
-  EXPECT_EQ(expected, actual);
-}
